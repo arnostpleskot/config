7a7817839a6be3db179750e9b65b710d7ef447eb
Arnost Pleskot <arnostpleskot@gmail.com>
2019-08-19 12:53:33 +0200

    move nvim to spacevim

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,13 @@
 source ~/.asdf/asdf.fish
-
 # if status is-interactive
 # and not set -q TMUX
 #    exec tmux
 # end
 # clear
 
+# OPAM configuration
+. /Users/arnost/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+
+# Global settings
+set -x LC_ALL en_US.UTF-8
+set -x theme_color_scheme terminal2
